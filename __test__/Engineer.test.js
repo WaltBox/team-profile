66c0a5a10fb907affc117f35d40195ea49652eb4
Walt Boxwell <waltboxwell@gmail.com>
2022-05-16 21:41:12 -0500

    Engineer test pass

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,20 @@
-test
+const { exportAllDeclaration } = require("@babel/types");
+const { default: TestRunner } = require("jest-runner");
+const { getMaxListeners } = require("process");
+const Engineer = require("../lib/Engineer");
+
+test("Initialize engineer object", () => {
+    const engineer = new Engineer('Walt', 1, "waltboxwell@gmail.com")
+})
+
+test("get github username", () => {
+    const github = 'waltbox'
+    const x = new Engineer("Walt", 1, "waltboxwell@gmail.com", 'waltbox');
+    expect(x.getGithub()).toBe(github);
+})
+
+test("get role of employee", () => {
+    const engineer = new Engineer("Walt", 1, "waltboxwell@gmail.com", 'waltbox');
+    console.log(engineer.getRole())
+    expect(engineer.getRole()).toBe("Engineer");
+})

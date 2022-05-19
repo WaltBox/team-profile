da2808456e39e4961075e0758a93e2063eac5202
Walt Boxwell <waltboxwell@gmail.com>
2022-05-19 13:00:40 -0500

    page not defined error

--- app.js
+++ app.js
@@ -1,86 +1,183 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-
+const path = require('path');
+const pageTemplate = require('./src/pageTemplate');
 
 const fs = require('fs');
 const inquirer = require('inquirer');
-
-const questions = [
+const teamArr = [];
+const engineerQuestion = [
     {
         type: 'input',
-        name:'managerName',
-        message: 'Who is the manager of your team?'
-    },
-
-    {
-        type: 'input',
-        name:'managerId',
-        message: 'What is the managers ID number?'
-    },
-
-    {
-        type: 'input',
-        name:'managerEmail',
-        message: 'What is the managers email?'
-    },
-
-    {
-        type: 'input',
-        name:'managerOfficeNumber',
-        message: 'What is the managers office number?'
-    },
-    {
-        type: 'checkbox',
-        name:'employeeRole',
-        message: 'What is the role of this employee?',
-        choices: ['Manager', 'Engineer', 'Intern']
+        name: 'engineerName',
+        message: 'Who is the Engineer of your team?'
     },
     {
         type: 'input',
-        name:'engineerName',
-        message: 'Who is the manager of your team?'
-    },
-
-    {
-        type: 'input',
-        name:'engineerId',
+        name: 'engineerId',
         message: 'What is the managers ID number?'
     },
     {
-        type:'input',
+        type: 'input',
         name: 'engineerEmail',
         message: 'What is the engineers email address?'
     },
     {
         type: 'input',
-        name:'engineerGithub',
+        name: 'engineerGithub',
         message: 'What is the Engineers github username?'
-    },
+    }
+]
+
+const internQuestions = [
     {
         type: 'input',
-        name:'internName',
-        message: 'Who is the manager of your team?'
+        name: 'internName',
+        message: 'Who is the Intern on your team?'
     },
 
     {
         type: 'input',
-        name:'internId',
+        name: 'internId',
         message: 'What is the interns ID number?'
     },
 
     {
         type: 'input',
-        name:'internEmail',
+        name: 'internEmail',
         message: 'What is the interns email address?'
     },
 
     {
         type: 'input',
-        name:'internSchool',
+        name: 'internSchool',
         message: 'Where did the intern go to school?'
+    }
+]
+
+const questions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'Who is the manager of your team?'
     },
+
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the managers ID number?'
+    },
+
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the managers email?'
+    },
+
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: 'What is the managers office number?'
+    },
+
+
+
 ];
+
+// Function write index.html
+
+function generateTeamPage(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), pageTemplate(data))
+}
+
+function addMember (){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'Would you like to add a new employee?',
+            choices: ['Engineer', 'Intern', 'Build Team']
+        }
+    ])
+    .then(answer => {
+        switch (userResponses.employeeRole) {
+            case 'Engineer':
+                createEngineer();
+                break;
+            case 'Intern':
+                createIntern();
+                break;
+            default:
+                buildTeam();
+
+        }
+    })
+}
+
+function createEngineer() {
+    inquirer
+        .prompt(engineerQuestion)
+        .then(answer => {
+            console.log(answer)
+            const engineer = new Engineer(answer.engineerName,answer.engineerId,answer.engineerEmail,answer.engineerGithub)
+            teamArr.push(engineer)
+            addMember();
+        })
+}
+
+function createIntern() {
+    inquirer
+        .prompt(internQuestions)
+        .then(answer => {
+            const intern = new Intern(answer.internName, answer.internId, answer.internEmail, answer.internSchool)
+            teamArr.push(intern)
+            addMember();
+        })
+}
+
+function buildTeam(){
+            generateTeamPage('./dist/index.html', teamArr);
+
+}
+
+function addMember (){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'Would you like to add a new employee?',
+            choices: ['Engineer', 'Intern', 'Build Team']
+        }
+    ])
+    .then(answer => {
+        switch (answer.employeeRole) {
+            case 'Engineer':
+                createEngineer();
+                break;
+            case 'Intern':
+                createIntern();
+                break;
+            default:
+                buildTeam();
+
+        }
+    })
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(userResponses => {
+            addMember();
+           
+           // console.log('...DONE BITCH');
+        })
+        .catch(err => console.error(err));
+}
+
+
+init();
 
 
 
---
+++ src/pageTemplate.js
@@ -0,0 +1,113 @@
+const generateManager = function (manager) {
+  return `
+  <div class= "row">
+  <div class ="col s12 m6">
+  <div class ="card blue-grey darken-1">
+  <div class ="card-content white-text"> 
+  <span class ="card-title"> Manager: ${manager.name} </span>
+  <p> ID number: ${manager.id} </p>
+  <p> Office Number: ${manager.officeNumber} 
+  </div>
+  <div class = "card-action">
+   <a href="${manager.email}">Email</a>
+   </div>
+   </div>
+   </div>
+   </div>
+  `
+}
+
+const generateEngineer = function (engineer) {
+ return `
+ <div class= "row">
+  <div class ="col s12 m6">
+  <div class ="card blue-grey darken-1">
+  <div class ="card-content white-text"> 
+  <span class ="card-title"> Engineer: ${engineer.name} </span>
+  <p> Engineer ID: ${engineer.id} </p>
+  </div>
+  <div class = "card-action">
+   <a href="${engineer.email}">Email</a>
+   <a href ="${engineer.github}"> Github</a>
+   </div>
+   </div>
+   </div>
+   </div>
+ `
+}
+
+const generateIntern = function (intern) {
+ return `
+ <div class= "row">
+  <div class ="col s12 m6">
+  <div class ="card blue-grey darken-1">
+  <div class ="card-content white-text"> 
+  <span class ="card-title"> Engineer: ${intern.name} </span>
+  <p> Engineer ID: ${intern.id} </p>
+  <p> School:${intern.school}
+  </div>
+  <div class = "card-action">
+   <a href="${intern.email}">Email</a>
+  
+   </div>
+   </div>
+   </div>
+   </div>
+   `
+}
+
+const generateHTML = (data) => {
+  pageArray = [];
+
+for (let i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
+
+  if (role === 'Manager') {
+    const managerCard = generateManager(employee);
+    pageArray.push(managerCard);
+  }
+  if (role === 'Engineer') {
+    const engineerCard = generateEngineer(employee);
+    pageArray.push(engineerCard);
+  }
+  if (role === 'Intern') {
+    const internCard = generateIntern(employee);
+    page.Array.push(internCard);
+  }
+}
+
+const employeeCards = pageArray.join('')
+
+const generateTeam= generateTeamPage(employeeCards);
+return generateTeam;
+}
+const generateTeamPage = function(employeeCards) {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    <div class="nav-wrapper">
+    <a href="#" class="brand-logo center"> Our team </a>
+    </div>
+    </nav>
+    <div class ="container">
+    ${employeeCards}
+    </div>
+
+</body>
+</html>
+
+`;
+}
+
+module.exports = generateHTML;
+
+
